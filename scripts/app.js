1cc8128994484d980dd9fa7855bb0dd9a6aeab91
sneizdecker <sneizdecker@gmail.com>
2016-06-12 08:41:33 -0400

    chore(project): update project

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,34 @@
 import $ from 'jquery';
-import slimscroll from 'fullpage.js/vendors/jquery.slimscroll.js';
-import fullpage from 'fullpage.js';
-
-// fullPage plugin initialization
-$('.sections').fullpage({
-	anchors: ['homepage', 'myWorks', 'mySkills', 'contactMe'],
-	sectionSelector: '.sections__item',
-	scrollOverflow: true,
-	verticalCentered: false,
-	scrollBar: false,
-	css3: true
-});
+import ScrollMagic from 'scrollmagic';
 
 // Hamburger and mobile menu toggle
 $('.hamburger').click(function () {
 	$(this).toggleClass('hamburger_active');
 	$('.mobile-menu').toggleClass('mobile-menu_active');
 });
+
+// Anchor navigation
+$(function () {
+	$('.menu__link').click(function () {
+		let target = $(this.hash);
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 400);
+				return false;
+			}
+		}
+	});
+});
+
+// ScrollMagic parallax
+const controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+	duration: 100,
+	offset: 0
+})
+.setPin('.hero')
+.addTo(controller);
